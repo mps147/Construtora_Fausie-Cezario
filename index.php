c974d6a89f5b9f4838cd26cb09ed1a21856ebba1
Marcelo Pereira dos Santos <marcelopereiraIFSP@gmail.com>
2016-06-16 16:19:39 -0300

    Adicionada arquivo sass e insersao do css no index

--- index.php
+++ index.php
@@ -3,6 +3,10 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Bem vindo - A Construtora Fausie e Cezario</title>
+
+	<!-- ./Inserindo a paginas de estilos css -->
+	<link rel="stylesheet" href="css/style.css">
+	<!-- \.Fim da inserção das paginas de estilo-->
 </head>
 <body>
 	
