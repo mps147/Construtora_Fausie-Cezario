95d482aa23280c84a9985ed5b4c8a72b9dba5cb8
Marcelo Pereira dos Santos <marcelopereiraIFSP@gmail.com>
2016-06-16 17:22:02 -0300

    Inicio do header com o logo, telefone e menu

--- index.php
+++ index.php
@@ -9,6 +9,32 @@
 	<!-- \.Fim da inserção das paginas de estilo-->
 </head>
 <body>
-	
+
+	<div id="content-main" class="col-md-8 col-md-offset-2"><!-- ./div com todo o conteudo da página -->
+
+		<header class="container-fluid"><<!-->./Logo e menu</!-->
+
+			<div id="img-logo" class="col-md-2 col-xs-12">
+				<img class="img-responsive" src="" alt="[image of logo top]">
+			</div>
+			
+			<div id="phone-top" class="col-md-2 col-md-push-right col-xs-12 col-xs-pull-6">
+				<i class="glyphicon glyphicon-phone"></i><p>(12) 3662-1411</p>
+			</div>
+
+			<nav><<!-->./menu of navigation</!-->
+				<ul>
+					<li><a href="#"> INÍCIO </a></li>
+					<li><a href="#"> A EMPRESA </a></li>
+					<li><a href="#"> SERVIÇOS </a></li>
+					<li><a href="#"> PROJETOS REALIZADOS </a></li>
+					<li><a href="#"> CONTATO </a></li>
+				</ul>
+			</nav><<!-->\.menu</!-->
+
+		</header><<!-->\.Logo e menu</!-->
+
+	</div><!-- \.div Content-main -->
+
 </body>
 </html>
