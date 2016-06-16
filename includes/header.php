22583e621f46cc6f5ba26cea130b6546f652d4a4
Marcelo Pereira dos Santos <marcelopereiraIFSP@gmail.com>
2016-06-16 18:05:31 -0300

    arrumando erros js e bootstrap

---
+++ includes/header.php
@@ -0,0 +1,21 @@
+<header class="container-fluid"><!-- Logo e menu -->
+
+	<div id="img-logo" class="col-md-2 col-xs-12">
+		<img class="img-responsive" src="" alt="[image of logo top]">
+	</div>
+	
+	<div id="phone-top" class="col-md-2 col-md-push-right col-xs-12">
+		<i class="glyphicon glyphicon-earphone"></i><p>(12) 3662-1411</p>
+	</div>
+
+	<nav><!-- /menu of navigation -->
+		<ul>
+			<li><a href="#"> INÍCIO </a></li>
+			<li><a href="#"> A EMPRESA </a></li>
+			<li><a href="#"> SERVIÇOS </a></li>
+			<li><a href="#"> PROJETOS REALIZADOS </a></li>
+			<li><a href="#"> CONTATO </a></li>
+		</ul>
+	</nav><!-- /menu -->
+
+</header><!-- /Logo e menu -->
--- index.php
+++ index.php
@@ -4,37 +4,22 @@
 	<meta charset="UTF-8">
 	<title>Bem vindo - A Construtora Fausie e Cezario</title>
 
-	<!-- ./Inserindo a paginas de estilos css -->
+	<!-- Inserindo a paginas de estilos css -->
+	<link rel="stylesheet" href="css/bootstrap.css">
 	<link rel="stylesheet" href="css/style.css">
-	<!-- \.Fim da inserção das paginas de estilo-->
+	<!-- /Fim da inserção das paginas de estilo-->
 </head>
 <body>
 
-	<div id="content-main" class="col-md-8 col-md-offset-2"><!-- ./div com todo o conteudo da página -->
+	<div id="content-main" class="col-md-8 col-md-offset-2"><!-- /div com todo o conteudo da página -->
 
-		<header class="container-fluid"><<!-->./Logo e menu</!-->
+		<?php include("includes/header.php"); ?>
 
-			<div id="img-logo" class="col-md-2 col-xs-12">
-				<img class="img-responsive" src="" alt="[image of logo top]">
-			</div>
-			
-			<div id="phone-top" class="col-md-2 col-md-push-right col-xs-12 col-xs-pull-6">
-				<i class="glyphicon glyphicon-phone"></i><p>(12) 3662-1411</p>
-			</div>
-
-			<nav><<!-->./menu of navigation</!-->
-				<ul>
-					<li><a href="#"> INÍCIO </a></li>
-					<li><a href="#"> A EMPRESA </a></li>
-					<li><a href="#"> SERVIÇOS </a></li>
-					<li><a href="#"> PROJETOS REALIZADOS </a></li>
-					<li><a href="#"> CONTATO </a></li>
-				</ul>
-			</nav><<!-->\.menu</!-->
-
-		</header><<!-->\.Logo e menu</!-->
-
-	</div><!-- \.div Content-main -->
-
+	</div><!-- /div Content-main -->
+	
+	<!-- JavaScripts -->
+	<script src="js/jquery.min.js"></script>
+	<script src="js/bootstrap.mim.js"></script>
+	<!-- /JavaScripts -->
 </body>
 </html>
