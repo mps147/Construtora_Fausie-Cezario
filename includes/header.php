e670f3fde76adae2bf5b0509051ab3438b377711
Marcelo Pereira dos Santos <marcelopereiraIFSP@gmail.com>
2016-06-17 18:05:29 -0300

    clean code

--- includes/header.php
+++ includes/header.php
@@ -5,8 +5,10 @@
 				<img src="./images/logo/logo_constructor.jpg" alt="[logo of construction company]">
 			</div><!-- /Logo -->
 
-			<div class="col-md-2 col-md-offset-7">
-				<p>(12) 3662-1411</p>
+			<div id="cotent-number">
+				<div id="content-number_phone" class="col-md-5 col-md-offset-2">
+					<p>(12) 3662-1411</p>
+				</div>
 			</div>
 		</div>
 	</div>
