1b859ad16796f1e9362f9ee89e1c8df45a04a59b
Marcelo Pereira dos Santos <marcelopereiraIFSP@gmail.com>
2016-06-20 16:16:04 -0300

    End galery

--- includes/galery.php
+++ includes/galery.php
@@ -1,15 +1,14 @@
 <div id="galery" class="container-fluid">
-	<div class="row">
-		<div class="col-md-4">
+	<div id="galery-items" class="row">
+		<div class="galery-item">
 			<img src="images/images_main/projetos_casa.jpg" alt="business projects" class="img-responsive">
 		</div>
 
-		<div id="galery-imgMid" class="col-md-4">
+		<div class="galery-item">
 			<img src="images/images_main/house_mid.jpg" alt="business tips" class="img-responsive">
 		</div>
-
-		<div class="col-md-4">
-			<img src="images/images_main/projects_performed.jpg" alt="dreams come true" class="img-responsive">
+		<div class="galery-item">
+			<img src="images/images_main/projects_performed.jpg" alt="business projects performed" class="img-responsive">
 		</div>
 	</div>
 </div>
