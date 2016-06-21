f11bbd7fc3d6c8bc8e3e14fa6fe831f09b9da605
Marcelo Pereira dos Santos <marcelopereiraIFSP@gmail.com>
2016-06-21 18:00:07 -0300

    ajuste na galeria

--- includes/galery.php
+++ includes/galery.php
@@ -1,5 +1,5 @@
 <div id="projects" class="container-fluid">
-	<div class="row" id="projects-item">
+	<div class="row">
 		<div class="projects__items"><img src="" alt="" class="img-responsive"></div>
 		<div class="projects__items"><img src="" alt="" class="img-responsive"></div>
 		<div class="projects__items"><img src="" alt="" class="img-responsive"></div>
